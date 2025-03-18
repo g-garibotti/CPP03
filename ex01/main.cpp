cb69620ddad672d9e38a2beaf2df9d95ea513bed
Genaro Garibotti <ggaribot@paul-f4Br3s7.clusters.42paris.fr>
2025-03-18 22:21:17 +0100

    ex01

---
+++ ex01/main.cpp
@@ -0,0 +1,23 @@
+#include "ClapTrap.hpp"
+#include "ScavTrap.hpp"
+#include <iostream>
+
+int main()
+{
+    std::cout << "==== Testing ClapTrap ====" << std::endl;
+    ClapTrap clapTrap("CT-001");
+    clapTrap.attack("Enemy Robot");
+    clapTrap.takeDamage(5);
+    clapTrap.beRepaired(3);
+    clapTrap.takeDamage(10);  
+    clapTrap.beRepaired(5);
+    
+    std::cout << "\n==== Testing ScavTrap ====" << std::endl;
+    ScavTrap scavTrap("ST-002");
+    scavTrap.attack("Enemy Mech");
+    scavTrap.takeDamage(30);
+    scavTrap.beRepaired(20);
+    scavTrap.guardGate();
+    std::cout << std::endl;
+    return 0;
+}
